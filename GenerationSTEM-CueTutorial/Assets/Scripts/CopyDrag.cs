6ea3501095e9fb65ec6371545ef577f49f2988a3
Stephanie <stephcarpin710@gmail.com>
2020-04-15 22:22:10 -0700

    updated prefabs and created copy of gameobjects

---
+++ GenerationSTEM-CueTutorial/Assets/Scripts/CopyDrag.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyDrag : MonoBehaviour
+{
+    RaycastHit hit;
+    public GameObject prefab;
+
+    void OnMouseDown()
+    {
+        GameObject obj = Instantiate(prefab, transform.position, Quaternion.identity) as GameObject;
+    }
+}
+
+//new Vector3(hit.point.x, hit.point.y, hit.point.z)
