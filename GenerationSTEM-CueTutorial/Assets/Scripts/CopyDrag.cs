a5e0e3f218770cda35fac8a93956fdd6077d4d1c
Stephanie <stephcarpin710@gmail.com>
2020-04-22 15:42:21 -0700

    smalll changes

--- GenerationSTEM-CueTutorial/Assets/Scripts/CopyDrag.cs
+++ GenerationSTEM-CueTutorial/Assets/Scripts/CopyDrag.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 
 public class CopyDrag : MonoBehaviour
 {
-    RaycastHit hit;
+    private bool isDragging;
     public GameObject prefab;
+    int click_counter = 0;
 
     void OnMouseDown()
     {
-        GameObject obj = Instantiate(prefab, transform.position, Quaternion.identity) as GameObject;
+        if (click_counter == 0)
+        {
+            GameObject obj = Instantiate(prefab, new Vector3(-1.5f, transform.position.y, transform.position.z), Quaternion.identity) as GameObject;
+            isDragging = true;
+            click_counter = +1;
+        }
+    }
+
+    public void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    void Update()
+    {
+        if (isDragging)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition);
+        }
     }
 }
 
