00f158aa095a4d42d93dd4148c6c99728cde4757
Stephanie <stephcarpin710@gmail.com>
2020-04-25 02:02:10 -0700

    moveable code & trash option

---
+++ GenerationSTEM-CueTutorial/Assets/Scripts/TrashOption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashOption : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //Debug.Log("collision name = " + col.gameObject.name);
+        Destroy(col.gameObject);
+
+    }
+
+}
