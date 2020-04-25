b74bbda7e3bfc744e974d1a840f14eeed212aaba
Stephanie <stephcarpin710@gmail.com>
2020-04-25 02:36:15 -0700

    all buttons and UI work

---
+++ GenerationSTEM-CueTutorial/Assets/Scripts/PlayStopButtonAction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayStopButtonAction : MonoBehaviour
+{
+    public void PlayBlockCode()
+    {
+        //will run the block code for the play button
+        Debug.Log("Play Button Clicked");
+    }
+
+    public void StopBlockCode()
+    {
+        //will run the block code for the stop button
+        Debug.Log("Stop Button Clicked");
+    }
+}
