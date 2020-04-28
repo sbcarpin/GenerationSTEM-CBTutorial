70f1647fa9dcb9576a95ab2f6330225d0577dc11
Stephanie <stephcarpin710@gmail.com>
2020-04-28 00:18:37 -0700

    start of each block with code

---
+++ GenerationSTEM-CueTutorial/Assets/Scripts/BlockCodeCode/MoveForward.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    public GameObject bike;
+    int distance; //will have to get user input for these two values
+    int speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //do these temporary values for now (until you can get user input)
+        distance = 10; //vectors?
+        speed = 0; //how should i do this? (figure out how to figure out time speed 1 equal 1 second )
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
