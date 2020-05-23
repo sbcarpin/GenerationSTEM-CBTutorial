63858c54f1aa32de0d29e24a5a366d380ba21491
Stephanie <stephcarpin710@gmail.com>
2020-05-22 20:00:03 -0700

    updated blocks

---
+++ GenerationSTEM-CueTutorial/Assets/Scripts/BlockCodeCode/turnneg90.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnneg90 : MonoBehaviour
+{
+    public GameObject bike;
+    private float rotationDegree;
+
+    /* only use this start for testing otherwise dont need it since code
+     * will play whe "Play" button is clicked */
+    void Start()
+    {
+        rotationDegree = 90f;
+        Turn();
+    }
+
+    void Turn()
+    {
+        bike.transform.Rotate(0, 0, -rotationDegree);
+    }
+}
